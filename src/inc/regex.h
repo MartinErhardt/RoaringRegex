9bd286a8cc8ef71727e68d2241df35c09c6a1a45
MartinErhardt <merhardt@rhrk.uni-kl.de>
2021-11-24 22:26:33 +0100

    memory pooling

--- src/inc/regex.h
+++ src/inc/regex.h
@@ -3,66 +3,170 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 #pragma once
 #include<vector>
+#include<memory>
 #include "../../CRoaring/roaring.hh"
 #include "../../CRoaring/roaring.c"
+#include<iostream>
 namespace Regex{
     typedef struct State{
-        Roaring tr[512]={};         //0-0xFF         forward transition
+        Roaring tr[512];         //0-0xFF         forward transition
                                     //0x100-0x1FF    backward transition(relevant for FRegex iterator dereference) Also relevant for "quick" NFA collocation
-        State (){}
-        State& operator=(const State& s){
-            for(int c=0;c<512;c++) tr[c]=s.tr[c];
-            return *this;
-        }
-        State& operator=(State&& s){
-            for(int c=0;c<512;c++) tr[c]=std::move(s.tr[c]);
-            return *this;
-        }
-        State (const State& s){
-            for(int c=0;c<512;c++) tr[c]=s.tr[c];
-        }
-        State (State&& s){
-            for(int c=0;c<512;c++) tr[c]=std::move(s.tr[c]);
-        }
+        //State (){
+        //}
+        State& operator=(const State& s)=delete;
+        State& operator=(State&& s)=delete;
+        State (const State& s)=delete;
+        State (State&& s)=delete;
+        ~State(){
+            for(int c=0;c<512;c++){
+                //std::cout<<"release"<<std::endl;
+                tr[c].~Roaring();
+            }
+        };
     }state;
+    
     enum operation{CONCATENATION,BRACKETS,OR};
-    class NFA{
-        template<bool fwd> NFA& shift(char c);
+    class PseudoNFA{
+    public:
+        uint32_t size;
+        uint32_t initial_state;
+        PseudoNFA(){};
+        PseudoNFA(const PseudoNFA& to_copy){size=to_copy.size;initial_state=to_copy.initial_state;}
+        PseudoNFA(PseudoNFA&& to_move){size=to_move.size; initial_state=to_move.initial_state; to_move.size=0;}
+        PseudoNFA(uint32_t cur_n,uint32_t states_n):size(states_n),initial_state(cur_n){};
+        PseudoNFA(uint32_t cur_n,uint32_t states_n,state* states){size=1;initial_state=cur_n;};         //create empty NFA
+        PseudoNFA(uint32_t cur_n,char c,uint32_t states_n,state* states){size=2;initial_state=cur_n;};  //create NFA accepting just c
+        PseudoNFA& operator=(PseudoNFA& other){size=other.size;initial_state=other.initial_state; return *this;}
+        PseudoNFA& operator=(PseudoNFA&& other){size=other.size;other.size=0; initial_state=other.initial_state; return *this;}
+        PseudoNFA& operator|=(PseudoNFA&& other){return *this|=other;};     //Union
+        PseudoNFA& operator*=(PseudoNFA&& other){
+            //std::cout<<"CALL  BASE1"<<std::endl;
+            return *this*=other;};    //Concatenation
+        PseudoNFA& operator|=(PseudoNFA& other){size+=other.size;return *this;};    //Union
+        PseudoNFA& operator*=(PseudoNFA& other){
+            std::cout<<"CALL  BASE2this size "<<size<<"\tother size: "<<other.size<<std::endl;size+=other.size;return *this;};        //Concatenation
+        PseudoNFA& operator* (unsigned int n){return *this;};                     //Kleene operator
+    };
+    class NFA: public PseudoNFA{
         template<bool fwd> void skip(uint32_t n,uint32_t k);
     public:
-        bool start_early=false;
-        std::vector<state>  states;
+        state* states;                                 //owned first by Parser then Executable
         Roaring    current_states;
         Roaring    final_states;
-        uint32_t   initial_state;
-        uint32_t*  gather_for_fastunion=nullptr;       //TODO make class
-        Roaring**  select_for_fastunion=nullptr;       //TODO make struct
-        /*
-        const std::unordered_map<operation, nfa_operation> op_eval {
-                {CONCATENATION, this.operator*=},
-                {OR, this.operator|=}
-        };*/
+        NFA(){};
         NFA(const NFA& to_copy);
         NFA(NFA&& to_move);
-        NFA(uint32_t cur_n);                                    //create empty NFA
-        NFA(uint32_t cur_n,char c);                             //create NFA accepting just c
+        NFA(uint32_t cur_n,uint32_t states_n,state* states);         //create empty NFA
+        NFA(uint32_t cur_n,char c,uint32_t states_n,state* states);  //create NFA accepting just c
         NFA& operator=(NFA& other);
         NFA& operator=(NFA&& other);
-        static NFA build_NFA(const char* p);
-        static NFA bracket_expression(const char* start, int* i,uint32_t* cur_n,int ps,const char *p);
         NFA& operator|=(NFA&& other){return *this|=other;};     //Union
         NFA& operator*=(NFA&& other){return *this*=other;};     //Concatenation
         NFA& operator|=(NFA& other);                            //Union
         NFA& operator*=(NFA& other);                            //Concatenation
         NFA& operator* (unsigned int n);    //Kleene operator
-        #define FLAG_ACCEPTING   (1<<1)
-        #define FLAG_INITIAL     (1<<2)
-        uint8_t operator*();
-        NFA& operator<<(char c);            //process forward
-        NFA& operator>>(char c);            //process in reverse order
-        ~NFA();
     };
-
+    /*
+    template<int states_n>
+    class mempool_ptr:public std::shared_ptr<void>{
+        int states_n;
+        mempool_ptr(void* ptr,)
+        ~mempool_ptr(){
+            for(state* states,cur_state=states=reinterpret_cast<state*>(memory_pool.get());cur_state<states+states_n;states++) states->~State();
+            this->~std::shared_ptr<void>();
+        }
+    }*/
+    class Executable{
+        void* memory_pool;
+        size_t memory_pool_size;
+        bool moved_from=false;
+    public: //only for debug purposes
+        size_t states_n;
+        NFA nfa;
+    private:
+        uint32_t*  gather_for_fastunion=nullptr;                // owned by memory_pool
+        Roaring**   select_for_fastunion=nullptr;                // owned by memory_pool
+        template<bool fwd> Executable& shift(char c);
+        public: //TODO make private
+            Executable():nfa(){};
+            Executable(void* memory_pool, size_t memory_pool_size,size_t states_n, NFA&& nfa,uint32_t* gather_for_fastunion,Roaring** select_for_fastunion):
+            memory_pool(memory_pool),memory_pool_size(memory_pool_size),states_n(states_n),nfa(nfa),
+            gather_for_fastunion(gather_for_fastunion),
+            select_for_fastunion(select_for_fastunion){};
+            Executable& operator=(Executable& other)
+            /*{
+                nfa=other.nfa;
+                memory_pool=malloc(other.memory_pool_size);
+                memcpy(memory_pool,other.memory_pool,memory_pool_size);
+                gather_for_fastunion=other.gather_for_fastunion;
+                select_for_fastunion=other.select_for_fastunion;
+                return *this;
+            }*/
+            =delete;
+            Executable& operator=(Executable&& other){
+                nfa=std::move(other.nfa);
+                memory_pool=other.memory_pool;
+                memory_pool_size=other.memory_pool_size;
+                states_n=other.states_n;
+                gather_for_fastunion=other.gather_for_fastunion;
+                select_for_fastunion=other.select_for_fastunion;
+                other.memory_pool=nullptr;
+                other.gather_for_fastunion=nullptr;
+                other.select_for_fastunion=nullptr;
+                other.moved_from=true;
+                //std::cout<<"move"<<std::endl;
+                return *this;
+            };/*
+            Executable& operator=(Executable other){
+                return (*this)=std::move(other);
+            };*/
+            Executable(Executable& exe) //{(*this)=exe;}
+            =delete;
+            Executable(Executable&& exe){(*this)=std::move(exe);};
+            #define FLAG_ACCEPTING   (1<<1)
+            #define FLAG_INITIAL     (1<<2)
+            uint8_t operator*();
+            Executable& operator<<(char c);            //process forward
+            Executable& operator>>(char c);            //process in reverse order
+            ~Executable(){
+                //std::cout<<"moved_from: "<<moved_from<<"states_n: "<<states_n<<std::endl;
+                if(!moved_from){
+                    state* states=(state*)memory_pool;
+                    state* cur_state=states;
+                    for(;cur_state<states+states_n;cur_state++){
+                    //    std::cout<<"state: "<<cur_state<<"sates+states_n"<<states+states_n<<std::endl;
+                        cur_state->~State();
+                    }
+                    free(memory_pool);
+                }
+            }
+    };
+    class Lexer{
+        uint32_t states_n=0;
+        state* states;
+        template<class NFA_t>
+        NFA_t bracket_expression(const char* start, int* i,uint32_t new_initial,int ps,const char *p);
+        template<class NFA_t>
+        NFA_t build_NFA(const char* p);
+    public:
+        Executable exec;
+        Lexer(const char* p){
+            states_n=build_NFA<PseudoNFA>(p).size;
+            size_t memory_pool_size=sizeof(state)*states_n+sizeof(Roaring*)*states_n+sizeof(uint32_t*)*states_n;
+            //std::cout<<"size of automaton: "<<states_n<<std::endl;
+            //std::shared_ptr<void> memory_pool=std::shared_ptr<void>(malloc(memory_pool_size),free);
+            void* memory_pool=malloc(memory_pool_size);
+            memset(static_cast<char*>(memory_pool),0,memory_pool_size); // conforms to strict-aliasing; initializes all Roaring bitmaps
+            
+            //std::cout<<"memory pool: "<<memory_pool<<"\tsize"<<memory_pool_size<<std::endl;
+            states=reinterpret_cast<state*>(memory_pool);
+            NFA nfa=build_NFA<NFA>(p);
+            uint32_t* gather_for_fastunion=reinterpret_cast<uint32_t*>(static_cast<char*>(memory_pool)+sizeof(state)*states_n);
+            Roaring** select_for_fastunion=reinterpret_cast<Roaring**>(static_cast<char*>(memory_pool)+sizeof(state)*states_n+sizeof(Roaring*)*states_n);
+            exec=std::move(Executable(memory_pool,memory_pool_size,states_n,std::move(nfa),gather_for_fastunion,select_for_fastunion));
+            //std::cout<<"exec states_n: "<<exec.states_n<<std::endl;
+        }
+    };
     //NFA operator+(NFA& nfa,int rotate);
     /*
     class FRegexIterator{
--- src/regex.cpp
+++ src/regex.cpp
@@ -11,93 +11,51 @@
 using namespace roaring;
 using namespace Regex;
 #define ALIGN 128
-NFA::NFA(NFA&& to_move):states(std::move(to_move.states)),
+NFA::NFA(NFA&& to_move): PseudoNFA(to_move),states(to_move.states),
                         current_states(std::move(to_move.current_states)),
-                        final_states(std::move(to_move.final_states)),
-                        initial_state(to_move.initial_state){
-    gather_for_fastunion=to_move.gather_for_fastunion;
-    select_for_fastunion=to_move.select_for_fastunion;
-    to_move.gather_for_fastunion=nullptr;
-    to_move.select_for_fastunion=nullptr;
+                        final_states(std::move(to_move.final_states)){
 }
-NFA::NFA(const NFA& to_copy): states(to_copy.states),
+NFA::NFA(const NFA& to_copy):PseudoNFA(to_copy), states(to_copy.states),
                         current_states(to_copy.current_states),
-                        final_states(to_copy.final_states),
-                        initial_state(to_copy.initial_state){
-    if(to_copy.gather_for_fastunion){
-        gather_for_fastunion=new (std::align_val_t{ALIGN}) uint32_t [states.size()];
-        select_for_fastunion=new (std::align_val_t{ALIGN}) Roaring*[states.size()];
-        memcpy(gather_for_fastunion, to_copy.gather_for_fastunion, states.size()*sizeof(uint32_t));
-        memcpy(select_for_fastunion, to_copy.select_for_fastunion, states.size()*sizeof(Roaring*));
-    }
+                        final_states(to_copy.final_states){
 }
 NFA& NFA::operator=(NFA&other){
+    this->PseudoNFA::operator=(other);
     states          =other.states;
-    initial_state   =other.initial_state;
     final_states    =other.final_states;
     current_states  =other.current_states;
-    if(other.gather_for_fastunion){
-        gather_for_fastunion=new (std::align_val_t{ALIGN}) uint32_t[states.size()];
-        select_for_fastunion=new (std::align_val_t{ALIGN}) Roaring*[states.size()];
-        memcpy(gather_for_fastunion, other.gather_for_fastunion, states.size()*sizeof(uint32_t));
-        memcpy(select_for_fastunion, other.select_for_fastunion, states.size()*sizeof(Roaring*));
-    }
     return *this;
 }
 NFA& NFA::operator=(NFA&& other){
-    states          =std::move(other.states);
+    this->PseudoNFA::operator=(other);
+    states          =other.states;
+    other.states=nullptr;
     current_states  =std::move(other.current_states);
     final_states    =std::move(other.final_states);
-    other.states.push_back(state{});
-    //std::cout<<"initial state: "<<std::endl;
     other.final_states=Roaring::bitmapOf(1,other.initial_state);
-    //std::cout<<"Success!"<<std::endl;
-    initial_state=other.initial_state;
-    gather_for_fastunion=other.gather_for_fastunion;
-    select_for_fastunion=other.select_for_fastunion;
-    other.gather_for_fastunion=nullptr;
-    other.select_for_fastunion=nullptr;
     return *this;
 }
-NFA::NFA(uint32_t cur_n):final_states(std::move(Roaring::bitmapOf(1,cur_n))),
-                         initial_state(cur_n){
-    states.push_back(state{});
+NFA::NFA(uint32_t cur_n,uint32_t states_n,state*states):PseudoNFA(cur_n,states_n,states),states(states),final_states(std::move(Roaring::bitmapOf(1,cur_n))){
 }
-NFA::NFA(uint32_t cur_n,char c):
-                        final_states(Roaring::bitmapOf(1,cur_n+1)),
-                        initial_state(cur_n){
+NFA::NFA(uint32_t cur_n,char c,uint32_t states_n, state* states):PseudoNFA(cur_n,c,states_n,states), states(states),
+                        final_states(Roaring::bitmapOf(1,cur_n+1)){
     std::cout<<"single character: "<<c<<"\tcur_n: "<<cur_n<<std::endl;
-    //std::cout<<"test: ";
-    //Roaring::bitmapOf(1,2,3,4,5,6).printf();
-    //std::cout<<std::endl;
-    state new_state1{};
-    state new_state2{};
-    new_state1.tr[(int)c]=Roaring::bitmapOf(1,cur_n+1);
-    new_state2.tr[0x100+(int)c]=Roaring::bitmapOf(1,cur_n);
-    states.push_back(std::move(new_state1));
-    states.push_back(std::move(new_state2));
+    states[cur_n].tr[(int)c].add(cur_n+1);
+    states[cur_n+1].tr[0x100+(int)c].add(cur_n);
 }
-
-NFA::~NFA(){
-    if(gather_for_fastunion){
-        //std::cout<<"gather: "<<gather_for_fastunion<<std::endl;
-        //std::cout<<"select: "<<select_for_fastunion<<std::endl;
-        operator delete[](gather_for_fastunion, std::align_val_t{ALIGN});
-        operator delete[](select_for_fastunion, std::align_val_t{ALIGN});
-    }
-}
-NFA NFA::bracket_expression(const char* start, int* i,uint32_t* cur_n,int ps,const char *p){
+template<class NFA_t>
+NFA_t Lexer::bracket_expression(const char* start, int* i,uint32_t new_initial,int ps,const char *p){
     bool escaped=false;
     while(*i<ps&&!(!(escaped^=(p[*i]=='\\'))&&p[(*i)++]==']'));
     if(*i==ps) throw std::runtime_error("invalid expression!");
-    return NFA((*cur_n)++);
+    return NFA_t(new_initial,states_n,states);
 }
 std::ostream& operator<<(std::ostream& out, NFA const& nfa){
     out<<"initial state: "<<nfa.initial_state<<std::endl;
     out<<"final states: ";
     nfa.final_states.printf();
     out<<std::endl;
-    for(uint32_t i=0;i<nfa.states.size();i++){
+    for(uint32_t i=0;i<nfa.size;i++){
         out<<"state: "<<i+nfa.initial_state<<std::endl;
         out<<"forward transitions: "<<std::endl;
         for(unsigned char c=0; c<0xFF;c++){
@@ -120,6 +78,10 @@ std::ostream& operator<<(std::ostream& out, NFA const& nfa){
     }
     return out;
 }
+std::ostream& operator<<(std::ostream& out, PseudoNFA const& pnfa){
+    out<<"size: "<<pnfa.size<<std::endl;
+    return out;
+}
 Roaring operator+(Roaring& set,int32_t rotate){
     Roaring ret;
     for(Roaring::const_iterator i = set.begin(); i != set.end(); i++)
@@ -127,41 +89,43 @@ Roaring operator+(Roaring& set,int32_t rotate){
     return ret;
 }
 NFA operator+(NFA& input,int rotate){
-    NFA ret(input.initial_state+rotate);
-    ret.states.reserve(input.states.size());
-    for (uint32_t i=0;i<input.states.size(); i++){
-        ret.states.push_back(state{});
+    NFA ret(input.initial_state+rotate,input.size,input.states);
+    for (uint32_t i=input.initial_state;i<input.initial_state+input.size; i++){
         for(uint32_t c=0;c<0x200;c++) ret.states[i].tr[c]=std::move(input.states[i].tr[c]+rotate);
     }
-    ret.final_states=input.final_states+rotate;
+    ret.final_states=std::move(input.final_states+rotate);
     return ret;
 }
-NFA NFA::build_NFA(const char* p){
+PseudoNFA operator+(PseudoNFA& input,int rotate){
+    return PseudoNFA(input.initial_state+rotate,input.size);
+}
+#define next_initial(stack) (stack.empty()?0:stack.top().initial_state+stack.top().size)
+template<class NFA_t>
+NFA_t Lexer::build_NFA(const char* p){
     bool escaped=false;
-    std::stack<NFA> nfas;
+    std::stack<NFA_t> nfas;
     std::stack<operation> ops;
     ops.push(CONCATENATION);
     int ps=strlen(p);
-    uint32_t cur_n=0;
     auto clear_stack=[&](){
-        NFA cur_nfa(std::move(nfas.top())); //FIXME not in printable state
+        NFA_t cur_nfa(std::move(nfas.top())); //FIXME not in printable state
         std::cout<<"clear stack"<< nfas.top()<<std::endl;
         if(ops.size()){
             ops.pop();
             std::cout<<"ops top: "<<ops.top()<<"\tnfas size: "<<nfas.size()<<std::endl;
             while(nfas.size()>1&&ops.top()!=BRACKETS){
-                cur_n-=nfas.top().states.size();
                 std::cout<<"################################# intermediate NFA: "<<std::endl;
                 std::cout<<cur_nfa;
                 std::cout<<"op: "<<ops.top()<<std::endl;
                 if(ops.top()==CONCATENATION){   
                     nfas.pop();
                     ops.pop();
+                    std::cout<<"size now: "<<nfas.top().size<<std::endl;
                     nfas.top()*=cur_nfa;
                     cur_nfa=std::move(nfas.top());
                 }
                 else if(ops.top()==OR){
-                    NFA intermediate(std::move(cur_nfa));
+                    NFA_t intermediate(std::move(cur_nfa));
                     ops.pop();
                     ops.pop();
                     nfas.pop();
@@ -174,11 +138,7 @@ NFA NFA::build_NFA(const char* p){
                         cur_nfa=std::move(nfas.top());
                     }
                     cur_nfa|=intermediate;
-                    //std::cout<<"################################# WTF? "<<std::endl;
-                    //std::cout<<cur_nfa;
                 }
-                //std::cout<<"################################# WTF? "<<std::endl;
-                //std::cout<<cur_nfa;
             }
             std::cout<<"################################# intermediate final NFA: "<<std::endl;
             std::cout<<cur_nfa;
@@ -188,9 +148,7 @@ NFA NFA::build_NFA(const char* p){
         nfas.push(std::move(cur_nfa));
     };
     auto repeat=[&](){
-        //std::cout<<"rotate PLS!"<<std::endl;
-        nfas.push(nfas.top()+(int32_t)nfas.top().states.size());
-        cur_n+=nfas.top().states.size();
+        nfas.push(nfas.top()+(int32_t)nfas.top().size);
         ops.push(CONCATENATION);
     };
     for(int i=0;i<ps;){
@@ -199,7 +157,7 @@ NFA NFA::build_NFA(const char* p){
             i++;
         } else switch(p[i]*(!escaped)) {
             case '[':
-                nfas.push(NFA::bracket_expression(&p[i],&i,&cur_n,ps,p));
+                nfas.push(Lexer::bracket_expression<NFA_t>(&p[i],&i,nfas.top().initial_state+nfas.top().size,ps,p));
                 ops.push(CONCATENATION);
                 break;
             case '(':
@@ -230,14 +188,14 @@ NFA NFA::build_NFA(const char* p){
                 i++;
                 break;
             case '?':
-                nfas.top()|=NFA(cur_n++);
+                nfas.top()|=NFA_t(next_initial(nfas),states_n,states);
                 //nfas.top().final_states.add(nfas.top().initial_state);
                 i++;
                 break;
             case '{':
             {
                 int j=i++;
-                NFA repeated(nfas.top());
+                NFA_t repeated(nfas.top());
                 while(i<ps&&!(p[i]!='}'||p[i]==',')) i++;
                 if(i==ps) throw std::runtime_error("invalid expression!");
                 int m=strtol(&p[j],nullptr,10);
@@ -251,82 +209,85 @@ NFA NFA::build_NFA(const char* p){
                         repeat();
                         nfas.top()*1;
                     } else if(n>m){
-                        repeated|=NFA(cur_n++);
-                        for(int k=m;k<n;k++) nfas.push(repeated);
+                        repeated|=NFA_t(next_initial(nfas),states_n,states);
+                        for(int k=m;k<n;k++){
+                            nfas.push(repeated);
+                            ops.push(CONCATENATION);
+                        }
                     }
                 }
                 break;
             }
             case '^':
             case '$':
-                nfas.push(NFA(cur_n,'\0'));
-                ops.push(CONCATENATION);
-                cur_n+=2;
+                nfas.push(NFA_t(next_initial(nfas),'\0',states_n,states));
+                ops.push(CONCATENATION); //states
+                i++;
                 break;
             default:
-                nfas.push(NFA(cur_n,p[i++]));
+                //uint32_t next_s=next_initial(nfas);
+                //if(!nfas.empty())std::cout<<"TOP initial"<<nfas.top().initial_state<<"TOP size"<<nfas.top().size<<"\tcur_n: "<<next_s<<std::endl;
+                nfas.push(NFA_t(next_initial(nfas),p[i++],states_n,states));
                 ops.push(CONCATENATION);
+                //std::cout<<"TOP2 initial"<<nfas.top().initial_state<<"TOP2 size"<<nfas.top().size<<"\tcur_n: "<<next_s<<std::endl;
                 //std::cout<<"################################# intermediate NFA: "<<p[i-1]<<std::endl;
                 //std::cout<<nfas.top();
-                //std::cout<<nfas.top();
-                cur_n+=2;
                 break;
         }
     }
     clear_stack();
     if(nfas.size()!=1) throw std::runtime_error("invalid expression");
-    NFA new_nfa=nfas.top();
+    NFA_t new_nfa=nfas.top();
     nfas.pop();
-    new_nfa.gather_for_fastunion=new(std::align_val_t{ALIGN}) uint32_t[new_nfa.states.size()];
-    new_nfa.select_for_fastunion=new(std::align_val_t{ALIGN}) Roaring*[new_nfa.states.size()];
     return new_nfa;
-}
+} //push_back
 #define idx(c,fwd) ([&]{                         \
           if constexpr (fwd) return ((size_t)c); \
           else return ((size_t)c)+0x100;         \
         }())
 template<bool fwd>
-NFA& NFA::shift(char c){
-    size_t current_states_cardinality=current_states.cardinality();
-    current_states.toUint32Array(gather_for_fastunion);
+Executable& Executable::shift(char c){
+    size_t current_states_cardinality=nfa.current_states.cardinality();
+    nfa.current_states.toUint32Array(gather_for_fastunion);
     for(uint32_t i=0;i<current_states_cardinality;i++)
-        select_for_fastunion[i]=&states[gather_for_fastunion[i]-initial_state].tr[idx(c,fwd)];
-    current_states = Roaring::fastunion(current_states_cardinality,
+        select_for_fastunion[i]=&nfa.states[gather_for_fastunion[i]].tr[idx(c,fwd)];
+    nfa.current_states = Roaring::fastunion(current_states_cardinality,
                                         const_cast<const Roaring**>(select_for_fastunion));
     return *this;
 }
-NFA& NFA::operator<<(char c)
+Executable& Executable::operator<<(char c)
 {
     return shift<true>(c);
 }
-NFA& NFA::operator>>(char c)
+Executable& Executable::operator>>(char c)
 {
     return shift<false>(c);
 }
-uint8_t NFA::operator*(){
-    return (current_states.contains(initial_state)<<1)|(current_states.and_cardinality(final_states)>0);
+uint8_t Executable::operator*(){
+    return (nfa.current_states.contains(nfa.initial_state)<<1)|(nfa.current_states.and_cardinality(nfa.final_states)>0);
 };
 template<bool fwd>
 void NFA::skip(uint32_t n,uint32_t k){
-    uint32_t to_skip=fwd?k-initial_state:n-initial_state;
-    uint32_t fixed=fwd?n-initial_state:k-initial_state;
+    uint32_t to_skip=fwd?k:n;
+    uint32_t fixed=fwd?n:k;
     //std::cout<<"to_skip: "<<to_skip<<"\tfixed: "<<fixed<<std::endl;
     for(unsigned char c=0;c<0xFF;c++){
         states[fixed].tr[idx(c,fwd)] |=states[to_skip].tr[idx(c,fwd)];
         for(Roaring::const_iterator j =states[to_skip].tr[idx(c,fwd)].begin();
                                     j!=states[to_skip].tr[idx(c,fwd)].end();j++){
             //std::cout<<"inbound: "<<*j;
-            states[*j-initial_state].tr[idx(c,!fwd)].add(fixed+initial_state);
+            states[*j].tr[idx(c,!fwd)].add(fixed);
         }
     }
 }
 NFA& NFA::operator*=(NFA& other){
+    this->PseudoNFA::operator*=(other);
+    
     std::cout<<"################################# merge NFA: "<<std::endl;
     std::cout<<other;
     std::cout<<"################################# merge into NFA: "<<std::endl;
     std::cout<<*this;
     //std::cout<<"concat1"<<std::endl;
-    states.insert(states.end(),other.states.begin(),other.states.end());
     //std::cout<<"concat2"<<std::endl;
     for(Roaring::const_iterator i = final_states.begin(); i != final_states.end(); i++)
         skip<false>(*i,other.initial_state);
@@ -338,12 +299,12 @@ NFA& NFA::operator*=(NFA& other){
     return *this;
 }
 NFA& NFA::operator|=(NFA& other){
+    this->PseudoNFA::operator|=(other);
     std::cout<<"################################# union NFA: "<<std::endl;
     std::cout<<other;
     std::cout<<"################################# union into NFA: "<<std::endl;
     std::cout<<*this;
     final_states|=other.final_states;
-    states.insert(states.end(),other.states.begin(),other.states.end());
     skip<true>(initial_state,other.initial_state);
     if(other.final_states.contains(other.initial_state))  final_states.add(initial_state);
     //std::cout<<"################################# merged to NFA: "<<std::endl;
@@ -368,9 +329,9 @@ int main(){
             return -1;
     text[strlen(text)-1]='\0';
     pattern[strlen(pattern)-1]='\0';
-    NFA my_nfa=NFA::build_NFA(pattern);
+    Lexer l(pattern);
     std::cout<<"################################# final NFA: "<<std::endl;
-    std::cout<<my_nfa;
+    std::cout<<l.exec.nfa;
     //std::cout<<"size: "<<sizeof(my_nfa)<<std::endl;
     free(text);
     free(pattern);
